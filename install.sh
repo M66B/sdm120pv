f7573861bb0ae4459732b22aa765aedaee03e5f6
M66B <M66B@users.noreply.github.com>
2024-03-11 15:12:15 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ln -s /data/etc/sdm120pv/service /service/sdm120pv
+
+#svstat /service/sdm120pv
+#tail -n 100 -f /data/log/sdm120pv/current | tai64nlocal
+#zip /tmp/sdm120pv.zip *.py ext/*.py *.ini *.sh service/run service/log/run
+
+#https://github.com/victronenergy/venus/wiki/howto-add-a-driver-to-Venus
+#https://github.com/mr-manuel/venus-os_dbus-mqtt-pv
+
+# nano /data/rc.local
+# ln -s /data/etc/sdm120pv/service /service/sdm120pv
+
+# nano /etc/udev/rules.d/serial-starter.rules
+# #ACTION=="add", ENV{ID_BUS}=="usb", ENV{ID_VENDOR_ID}=="1a86", ENV{ID_MODEL_ID}=="7523", ENV{VE_SERVICE}="cgwacs:default"
+# ACTION=="add", ENV{ID_BUS}=="usb", ENV{ID_MODEL}=="USB_Serial" ENV{VE_SERVICE}="ignore"
